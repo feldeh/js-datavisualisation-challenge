280065a22d8a40405d3e89fc537cb0b9bb89115c
fel <felicien.dehertogh@protonmail.com>
2022-06-26 17:54:25 +0200

    Remove id attribute from canvas

--- assets/script.js
+++ assets/script.js
@@ -3,108 +3,163 @@
 const table1 = document.getElementById('table1')
 const table2 = document.getElementById('table2')
 
-
-// create canvas
-
-const table1Canvas = document.createElement('canvas')
-table1Canvas.setAttribute('id', 'table1Canvas')
-
-table1.before(table1Canvas)
+// random color function
+const randHexColor = () => `#${Math.floor(Math.random() * Math.pow(16, 6)).toString(16)}`
 
 
-
-
-
-
-const ctx = table1Canvas.getContext('2d');
-
-var tableYear = [2002, 2003, 2004, 2005];
-
-new Chart(ctx, {
-    type: "line",
-    data: {
-        labels: tableYear,
-        datasets: [{
-            data: [860, 1140, 1060, 1060],
-            label: "country1",
-            borderColor: "red",
-            fill: false
-        }, {
-            data: [1600, 1700, 1700, 1900],
-            label: "country2",
-            borderColor: "green",
-            fill: false
-        }, {
-            data: [300, 700, 2000, 5000],
-            label: "country3",
-            borderColor: "blue",
-            fill: false
-        }]
-    },
-    options: {
-        legend: { display: false }
-    }
-});
-
-
-
-// retrieve data from ta ble1
-
-
-
-let table1Value = []
-let table1Countries = []
-let table1Year = []
-
-
-
-
-
-
-
-const getTable1Data = (table) => {
+const table1Chart = () => {
     const years = []
     const countryNames = []
     const tableData = []
-    const chartDatasets = []
-    const randHexColor = () => `#${Math.floor(Math.random() * Math.pow(16, 6)).toString(16)}`
+    const tableDatasets = []
 
-    for (let i = 2; i < table.getElementsByTagName('tr')[1].getElementsByTagName('th').length; i++) {
-        years.push(table.getElementsByTagName('tr')[1].getElementsByTagName('th')[i].innerText)
+    // retrieve years data
+    for (let i = 2; i < table1.getElementsByTagName('tr')[1].getElementsByTagName('th').length; i++) {
+        years.push(table1.getElementsByTagName('tr')[1].getElementsByTagName('th')[i].innerText)
     }
 
-
-    for (let i = 1; i < table.querySelectorAll('tbody> tr').length; i++) {
+    // retrieve datasets for each country
+    for (let i = 1; i < table1.querySelectorAll('tbody> tr').length; i++) {
         const h = i - 1
         tableData[h] = []
         countryNames[h] = []
-        chartDatasets[h] = []
-        for (let j = 0; j < table.querySelectorAll('tbody> tr')[i].querySelectorAll('td').length; j++) {
+        tableDatasets[h] = []
+        for (let j = 0; j < table1.querySelectorAll('tbody> tr')[i].querySelectorAll('td').length; j++) {
 
             if (j == 0) {
-                countryNames[h].push(table.querySelectorAll('tbody> tr')[i].querySelectorAll('td')[j].innerText)
+                countryNames[h].push(table1.querySelectorAll('tbody> tr')[i].querySelectorAll('td')[j].innerText)
             }
             else {
-                tableData[h].push(parseFloat((table.querySelectorAll('tbody> tr')[i].querySelectorAll('td')[j].innerText.replace(/,/, '.'))))
+                tableData[h].push(parseFloat((table1.querySelectorAll('tbody> tr')[i].querySelectorAll('td')[j].innerText.replace(',', '.'))))
             }
-            chartDatasets[h] = { data: tableData[h], label: countryNames[h], borderColor: randHexColor(), fill: false }
+            const color = randHexColor()
+            tableDatasets[h] = { data: tableData[h], label: countryNames[h], borderColor: color, backgroundColor: color, fill: false }
 
         }
 
     }
 
-    console.log("years", years)
-    console.log("tableData", tableData)
-    console.log("countryNames", countryNames)
-    console.log("chartDatasets", chartDatasets)
+    console.log("table1 years", years)
+    console.log("table1 tableData", tableData)
+    console.log("table1 countryNames", countryNames)
+    console.log("table1 tableDatasets", tableDatasets)
+
+    // create canvas element above table
+    const table1Canvas = document.createElement('canvas')
+    table1Canvas.setAttribute('height', '300px')
+    table1.before(table1Canvas)
+
+    // configure and generate chart with table data
+    const ctx = table1Canvas.getContext('2d');
+
+    new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: years,
+            datasets: tableDatasets
+        },
+        options: {
+            responsive: true,
+            plugins: {
+                title: {
+                    display: true,
+                    font: {
+                        weight: 'bold',
+                        size: '18px',
+                    },
+                    text: 'Offences recorded by the police, 2002-12'
+                }
+            },
+            scales: {
+                x: {
+                    display: true,
+                },
+                y: {
+                    display: true,
+                    type: 'logarithmic',
+                }
+            }
+        },
+    })
 }
 
-getTable1Data(table1)
+table1Chart()
+
+const table2Chart = () => {
+
+    const years = []
+    const countryNames = []
+    const tableData = []
+    const tableDatasets = []
+
+    // retrieve years values data
+    for (let i = 2; i < table2.getElementsByTagName('thead')[0].getElementsByTagName('th').length; i++) {
+        years.push(table2.getElementsByTagName('thead')[0].getElementsByTagName('th')[i].innerText)
+    }
+
+    // retrieve datasets for each country
+    for (let i = 0; i < table2.querySelectorAll('tbody> tr').length; i++) {
+        tableData[i] = []
+        countryNames[i] = []
+        tableDatasets[i] = []
+        for (let j = 0; j < table2.querySelectorAll('tbody> tr')[i].querySelectorAll('td').length; j++) {
+
+            if (j == 0) {
+                countryNames[i].push(table2.querySelectorAll('tbody> tr')[i].querySelectorAll('td')[j].innerText)
+            }
+            else {
+                tableData[i].push((table2.querySelectorAll('tbody> tr')[i].querySelectorAll('td')[j].innerText))
+            }
+            tableDatasets[i] = { data: tableData[i], label: countryNames[i], backgroundColor: randHexColor() }
+
+        }
+
+    }
+    console.log("table2 years", years)
+    console.log("table2 tableData", tableData)
+    console.log("table2 countryNames", countryNames)
+    console.log("table2 tableDatasets", tableDatasets)
+
+    // create canvas element above table
+    const table2Canvas = document.createElement('canvas')
+    table2Canvas.setAttribute('height', '250px')
+    table2.before(table2Canvas)
+
+    // configure and generate chart with table data
+    const ctx = table2Canvas.getContext('2d');
+
+    new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: years,
+            datasets: tableDatasets
+        },
+        options: {
+            responsive: true,
+            plugins: {
+                title: {
+                    display: true,
+                    font: {
+                        weight: 'bold',
+                        size: '18px',
+                    },
+                    text: 'Homicide, average per year, 2007-09 and 2010-12'
+                }
+            },
+        }
+    });
+}
+
+table2Chart()
 
 
-; (() => {
 
 
 
 
-})()
+    ; (() => {
+
+
+
+
+    })()
