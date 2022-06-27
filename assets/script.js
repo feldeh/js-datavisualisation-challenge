6d6fbe68df198d231ff774a9b2df5691f47a2df4
fel <felicien.dehertogh@protonmail.com>
2022-06-27 04:26:42 +0200

    Add live chart

--- assets/script.js
+++ assets/script.js
@@ -3,63 +3,52 @@
 const table1 = document.getElementById('table1')
 const table2 = document.getElementById('table2')
 
-// random color function
 const randHexColor = () => `#${Math.floor(Math.random() * Math.pow(16, 6)).toString(16)}`
 
 
 const table1Chart = () => {
-    const years = []
+    const tableData = {
+        labels: [],
+        datasets: []
+    }
     const countryNames = []
-    const tableData = []
-    const tableDatasets = []
+    const crimeData = []
 
-    // retrieve years data
+
     for (let i = 2; i < table1.getElementsByTagName('tr')[1].getElementsByTagName('th').length; i++) {
-        years.push(table1.getElementsByTagName('tr')[1].getElementsByTagName('th')[i].innerText)
+        tableData.labels.push(table1.getElementsByTagName('tr')[1].getElementsByTagName('th')[i].innerText)
     }
 
-    // retrieve datasets for each country
     for (let i = 1; i < table1.querySelectorAll('tbody> tr').length; i++) {
         const h = i - 1
-        tableData[h] = []
+        crimeData[h] = []
         countryNames[h] = []
-        tableDatasets[h] = []
+        tableData.datasets[h] = []
         for (let j = 0; j < table1.querySelectorAll('tbody> tr')[i].querySelectorAll('td').length; j++) {
 
             if (j == 0) {
                 countryNames[h].push(table1.querySelectorAll('tbody> tr')[i].querySelectorAll('td')[j].innerText)
             }
             else {
-                tableData[h].push(parseFloat((table1.querySelectorAll('tbody> tr')[i].querySelectorAll('td')[j].innerText.replace(',', '.'))))
+                crimeData[h].push(parseFloat((table1.querySelectorAll('tbody> tr')[i].querySelectorAll('td')[j].innerText.replace(',', '.'))))
             }
             const color = randHexColor()
-            tableDatasets[h] = { data: tableData[h], label: countryNames[h], borderColor: color, backgroundColor: color, fill: false }
+            tableData.datasets[h] = { data: crimeData[h], label: countryNames[h], borderColor: color, backgroundColor: color, fill: false }
 
         }
 
     }
 
-    console.log("table1 years", years)
-    console.log("table1 tableData", tableData)
-    console.log("table1 countryNames", countryNames)
-    console.log("table1 tableDatasets", tableDatasets)
 
-    // create canvas element above table
+
     const table1Canvas = document.createElement('canvas')
     table1Canvas.setAttribute('height', '300px')
     table1.before(table1Canvas)
 
-    // configure and generate chart with table data
-    const ctx = table1Canvas.getContext('2d');
-
-    new Chart(ctx, {
+    new Chart(table1Canvas, {
         type: 'line',
-        data: {
-            labels: years,
-            datasets: tableDatasets
-        },
+        data: tableData,
         options: {
-            responsive: true,
             plugins: {
                 title: {
                     display: true,
@@ -87,53 +76,43 @@ table1Chart()
 
 const table2Chart = () => {
 
-    const years = []
+    const tableData = {
+        labels: [],
+        datasets: []
+    }
     const countryNames = []
-    const tableData = []
-    const tableDatasets = []
+    const homicideData = []
 
-    // retrieve years values data
     for (let i = 2; i < table2.getElementsByTagName('thead')[0].getElementsByTagName('th').length; i++) {
-        years.push(table2.getElementsByTagName('thead')[0].getElementsByTagName('th')[i].innerText)
+        tableData.labels.push(table2.getElementsByTagName('thead')[0].getElementsByTagName('th')[i].innerText)
     }
 
-    // retrieve datasets for each country
     for (let i = 0; i < table2.querySelectorAll('tbody> tr').length; i++) {
-        tableData[i] = []
+        homicideData[i] = []
         countryNames[i] = []
-        tableDatasets[i] = []
         for (let j = 0; j < table2.querySelectorAll('tbody> tr')[i].querySelectorAll('td').length; j++) {
 
             if (j == 0) {
                 countryNames[i].push(table2.querySelectorAll('tbody> tr')[i].querySelectorAll('td')[j].innerText)
             }
             else {
-                tableData[i].push((table2.querySelectorAll('tbody> tr')[i].querySelectorAll('td')[j].innerText))
+                homicideData[i].push((table2.querySelectorAll('tbody> tr')[i].querySelectorAll('td')[j].innerText))
             }
-            tableDatasets[i] = { data: tableData[i], label: countryNames[i], backgroundColor: randHexColor() }
+            tableData.datasets[i] = { data: homicideData[i], label: countryNames[i], backgroundColor: randHexColor() }
 
         }
 
     }
-    console.log("table2 years", years)
-    console.log("table2 tableData", tableData)
-    console.log("table2 countryNames", countryNames)
-    console.log("table2 tableDatasets", tableDatasets)
 
-    // create canvas element above table
+
     const table2Canvas = document.createElement('canvas')
     table2Canvas.setAttribute('height', '250px')
     table2.before(table2Canvas)
 
-    // configure and generate chart with table data
-    const ctx = table2Canvas.getContext('2d');
 
-    new Chart(ctx, {
+    new Chart(table2Canvas, {
         type: 'bar',
-        data: {
-            labels: years,
-            datasets: tableDatasets
-        },
+        data: tableData,
         options: {
             responsive: true,
             plugins: {
@@ -157,9 +136,116 @@ table2Chart()
 
 
 
-    ; (() => {
+
+// loadXhrRemoteData = () => {
+
+//     let xhr = new XMLHttpRequest()
+
+//     xhr.open('GET', 'https://canvasjs.com/services/data/datapoints.php?xstart=1&ystart=10&length=10&type=json', true)
+
+//     xhr.onload = function () {
+//         if (xhr.status == 200) {
+//             let data = JSON.parse(this.responseText)
+//             console.log('REMOTE', data)
+//         }
+//     }
+
+//     xhr.send()
+
+
+// }
+
+// loadXhrRemoteData()
 
 
 
 
-    })()
+
+const table3Canvas = document.createElement('canvas')
+table3Canvas.setAttribute('background-color', 'red')
+document.getElementById('bodyContent').before(table3Canvas)
+
+
+const config = {
+    type: 'line',
+    data: {
+        labels: [],
+        datasets: [{
+            data: [],
+            label: "Live Data",
+            borderColor: "rgb(62,149,205)",
+            backgroundColor: "rgb(62,149,205,0.1)",
+        },
+        ]
+    }
+}
+
+
+
+
+const liveChart = new Chart(table3Canvas, config)
+
+const chartRefresh = (chart, yData, xData) => {
+    chart.data.labels = yData
+    chart.data.datasets.forEach(element => {
+        element.data = xData
+    })
+    chart.update()
+}
+
+let yData = []
+
+setInterval(function fetchData() {
+    fetch('https://canvasjs.com/services/data/datapoints.php?xstart=1&ystart=10&length=10&type=json', {
+        cache: "reload",
+    })
+
+        .then(response => response.json())
+        .then(fetchedData => {
+
+            for (let i = 0; i < fetchedData.length; i++) {
+                yData[i] = fetchedData[i][0]
+            }
+            chartRefresh(liveChart, yData, fetchedData)
+
+        })
+
+}, 1000);
+
+// let counter = 0
+
+// setInterval(function fetchData() {
+//     fetch('https://canvasjs.com/services/data/datapoints.php?xstart=1&ystart=10&length=10&type=json', {
+//         cache: "reload",
+//     })
+
+//         .then(response => response.json())
+//         .then(data => {
+
+//             data.forEach((element) => {
+//                 y.push(counter)
+//                 counter++
+//                 x.push(element[1])
+//             })
+
+//             liveChart.config.data.labels = y
+//             liveChart.config.data.datasets[0].data = x
+//             liveChart.update()
+//         })
+// }, 2000);
+
+
+
+
+
+
+
+
+
+
+; (() => {
+
+
+
+
+})()
